7a8502e55ffb4bbb44538b7ddba6dfa3f6f4f2c2
skystar-p <skystar@skystar.dev>
2021-06-23 21:23:23 +0900

    section

--- ebpf/drop.c
+++ ebpf/drop.c
@@ -4,7 +4,8 @@
 #include <linux/swab.h>
 #include <linux/pkt_cls.h>
 
-__section("classifier_egress_drop") int egress_drop(struct __sk_buff *skb) {
+__section("classifier_egress_drop")
+int egress_drop(struct __sk_buff *skb) {
 	void *data_end = (void *)(unsigned long long)skb->data_end;
 	void *data = (void *)(unsigned long long)skb->data;
 	struct ethhdr *eth = data;
@@ -20,7 +21,8 @@ __section("classifier_egress_drop") int egress_drop(struct __sk_buff *skb) {
 	return TC_ACT_OK;
 }
 
-__section("classifier_ingress_drop") int ingress_drop(struct __sk_buff *skb) {
+__section("classifier_ingress_drop")
+int ingress_drop(struct __sk_buff *skb) {
 	void *data_end = (void *)(unsigned long long)skb->data_end;
 	void *data = (void *)(unsigned long long)skb->data;
 	struct ethhdr *eth = data;
